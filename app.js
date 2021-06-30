6682a659700e608e449f675e751b2165ea595d74
Stephen Harrold <steharrold@gmail.com>
2021-06-30 16:57:24 +0100

    finalised chasing core game mechanic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var grid = document.getElementById('game-box');
 var gridHeight = 20;
 var gridWidth = 10;
+var moves = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
     // creates the game grid once the page has been loaded
@@ -16,8 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 var cell = document.createElement('div');
                 cell.setAttribute('class', 'game-box-cell black-outline empty');
                 // add some coordinates for movement later
-                cell.dataset.x = i;
-                cell.dataset.y = j;
+                cell.dataset.x = j;
+                cell.dataset.y = i;
                 row.appendChild(cell);
             }
             // add row to game-box
@@ -25,11 +26,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
     createGameGrid();
-    drawBlock();
+    drawBlockGoodie();
+    drawBlockBaddie();
 });
 
-
-// controls for moving blocks
+// controls for moving goodie
 document.addEventListener('keydown', (event) => {
     var xcoord;
     var ycoord;
@@ -38,78 +39,124 @@ document.addEventListener('keydown', (event) => {
     // get the coordinates of the block
     let gameState = getGameState();
     for (let i = 0; i < gameState.length; i++) {
-        ycoord = gameState[i].indexOf(1);
-        if (ycoord != -1) {
-            xcoord = i;
+        xcoord = gameState[i].indexOf(1);
+        if (xcoord != -1) {
+            ycoord = i;
             break;
         }
     }
     // move them down
     coords = [xcoord, ycoord];
     if (button === "ArrowDown") {
-        move(coords, "down");
+        move(coords, "down", "goodie");
     } else if (button === "ArrowUp") {
-        move(coords, "up");
+        move(coords, "up", "goodie");
     } else if (button === "ArrowLeft") {
-        move(coords, "left");
+        move(coords, "left", "goodie");
     } else if (button === "ArrowRight") {
-        move(coords, "right");
+        move(coords, "right", "goodie");
+    }
+    moves++;
+    var xcoordBad;
+    var ycoordBad;
+    var coordsBad = [];
+
+    if (moves % 2 == 0) {    // get the coordinates of the Baddie
+        for (let i = 0; i <= gameState.length; i++) {
+            xcoordBad = gameState[i].indexOf(2);
+            if (xcoordBad != -1) {
+                ycoordBad = i;
+                break;
+            }
+        }
+        coordsBad = [xcoordBad, ycoordBad];
+        var direction = calcNextBaddieMove(coords, coordsBad);
+        move(coordsBad, direction, "baddie");
     }
 }, false);
 
-function drawBlock() {
-    //draws a basic block at the starter square
-    var position = [0, gridWidth / 2];
-    fillCell(position[0], position[1]);
+// calculate distance from goodie
+function calcNextBaddieMove(coordsGood, coordsBad) {
+    var xDist;
+    var yDist;
+    xDist = coordsGood[0] - coordsBad[0];
+    yDist = coordsGood[1] - coordsBad[1];
+
+    console.log(Math.abs(xDist));
+    console.log(Math.abs(yDist));
+
+    if (Math.abs(xDist) > Math.abs(yDist)) {
+        if (xDist > 0) {
+            return "right";
+        } else if (xDist < 0) {
+            return "left";
+        }
+    } else if (Math.abs(xDist) < Math.abs(yDist)) {
+        if (yDist > 0) {
+            return "down";
+        } else if (yDist < 0) {
+            return "up";
+        }
+    }
 }
 
-function moveDown(coord) {
-    // to move down, empty the cell at coordinates
-    // let oldCell = document.querySelector(`[data-x=${coord[0]}] [data-y=${coord[1]}]`);
-    emptyCell(coord[0], coord[1]);
-    // increment the row value
-    coord[0]++;
-    // fill cell at incremented value
-    fillCell(coord[0], coord[1]);
+function drawBlockGoodie() {
+    //draws a basic block at the starter square
+    var position = [gridWidth / 2, 0];
+    fillCell(position[0], position[1], "goodie");
 }
-function move(coord, direction) {
+function drawBlockBaddie() {
+    //draws a basic block at the starter square
+    var position = [gridWidth / 2, gridHeight - 1];
+    fillCell(position[0], position[1], "baddie");
+}
+
+function move(coord, direction, characterType) {
     // to move, empty the cell at coordinates
-    // let oldCell = document.querySelector(`[data-x=${coord[0]}] [data-y=${coord[1]}]`);
-    emptyCell(coord[0], coord[1]);
-    let newX;
-    let newY;
+    emptyCell(coord[0], coord[1], characterType);
     switch (direction) {
         case "up":
             // decrement the row value
-            coord[0]--;
+            coord[1]--;
             break;
         case "down":
             // increment the row value
-            coord[0]++;
+            coord[1]++;
             break;
         case "left":
             // increment the column value
-            coord[1]--;
+            coord[0]--;
             break;
         case "right":
             // increment the column value
-            coord[1]++;
+            coord[0]++;
             break;
     }
     // fill cell at incremented value
-    fillCell(coord[0], coord[1]);
+    fillCell(coord[0], coord[1], characterType);
 }
 
-function emptyCell(cellRowIndex, cellNumberIndex) {
-    let targetCell = document.querySelector(`[data-x='${cellRowIndex}'][data-y='${cellNumberIndex}']`)
-    targetCell.classList.remove("filled");
+function emptyCell(cellXCoord, cellYCoord, characterType = "none") {
+    let targetCell = document.querySelector(`[data-x='${cellXCoord}'][data-y='${cellYCoord}']`)
+
+    if (characterType !== "none") {
+        targetCell.classList.remove(characterType);
+    } else {
+        targetCell.classList.remove("filled");
+    }
+
     targetCell.classList.add("empty");
 }
 
-function fillCell(cellRowIndex, cellNumberIndex) {
-    let targetCell = document.querySelector(`[data-x='${cellRowIndex}'][data-y='${cellNumberIndex}']`)
-    targetCell.classList.add("empty");
-    targetCell.classList.remove("filled");
+function fillCell(cellXCoord, cellYCoord, characterType = "none") {
+    let targetCell = document.querySelector(`[data-x='${cellXCoord}'][data-y='${cellYCoord}']`)
+    targetCell.classList.remove("empty");
+
+    if (characterType !== "none") {
+        targetCell.classList.add(characterType);
+    } else {
+        targetCell.classList.add("filled");
+    }
 }
 
 function getGameState() {
@@ -122,8 +169,10 @@ function getGameState() {
         Array.from(row.children).forEach(function (cell) {
             if (cell.className.includes('empty')) {
                 gameRow.push(0);
-            } else {
+            } else if (cell.className.includes('goodie')) {
                 gameRow.push(1);
+            } else if (cell.className.includes('baddie')) {
+                gameRow.push(2);
             }
         });
         gameState.push(gameRow);
